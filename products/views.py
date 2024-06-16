81c091470134198fd1ab0837df9b68fb7f5e3388
MARIA FERNANDA DIAS PADILHA <mdpadilha90@gmail.com>
2024-06-16 22:40:36 +0100

    Fix Views on Retirements

--- products/views.py
+++ products/views.py
@@ -260,7 +260,7 @@ def retirements(request):
         return HttpResponse('Form submitted successfully!')
     else:
         # Handle GET request or invalid form submission
-        return HttpResponse('Invalid request!')
+        return render(request, 'retirements.html')
 
 
 def occasions(request):
