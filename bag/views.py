b562401cfb4ce4e61ad4a46172d9dc64e4dbdf52
MARIA FERNANDA DIAS PADILHA <mdpadilha90@gmail.com>
2024-06-21 22:28:07 +0100

    Added Removing products

--- bag/views.py
+++ bag/views.py
@@ -6,3 +6,43 @@ def view_bag(request):
     """ A view that tenders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    
+    bag = request.session.get('bag', {})
+    if item_id in bag:
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+    
+    request.session['bag'] = bag
+    return redirect(redirect_url)
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product in the shopping bag"""
+    quantity = int(request.POST.get('quantity'))
+    
+    bag = request.session.get('bag', {})
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id, None)
+    
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+    try:
+        bag = request.session.get('bag', {})
+        if item_id in bag:
+            bag.pop(item_id)
+            request.session['bag'] = bag
+            return HttpResponse(status=200)
+        else:
+            return HttpResponse(status=404)
+    except Exception as e:
+        return HttpResponse(status=500)
