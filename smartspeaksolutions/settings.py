86f5e7396299ee06c5b36862b71ab69fc8852b29
MARIA FERNANDA DIAS PADILHA <mdpadilha90@gmail.com>
2024-06-21 18:57:11 +0100

    Added shopping bag app, urls, and templates

--- smartspeaksolutions/settings.py
+++ smartspeaksolutions/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
     'home',
     'products',
+    'bag',
 ]
 
 MIDDLEWARE = [
--- smartspeaksolutions/urls.py
+++ smartspeaksolutions/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('home/', include('home.urls')),
     path('products/', include('products.urls')),
+    path('bag/', include('bag.urls')),
     path('', include('home.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
