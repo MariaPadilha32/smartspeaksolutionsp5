6e330d5b0f5015e01e55ed5abea81c6e4f4921b3
MARIA FERNANDA DIAS PADILHA <mdpadilha90@gmail.com>
2024-06-22 13:06:01 +0100

    Create Checkout app, views, urls, template, css, models

--- smartspeaksolutions/settings.py
+++ smartspeaksolutions/settings.py
@@ -46,6 +46,10 @@ INSTALLED_APPS = [
     'home',
     'products',
     'bag',
+    'checkout',
+
+    #Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -61,6 +65,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'smartspeaksolutions.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -75,8 +81,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                #'django..template.context_procssors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
